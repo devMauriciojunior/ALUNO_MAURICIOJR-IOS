b672d7d6e8a47230a76bf4b125b6e853da1dd1a1
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-05-10 11:51:09 -0300

    Javascript

---
+++ JS/1° Aula - Introdução a Lógica/script.js
@@ -0,0 +1,21 @@
+// var p1 = 8; var p2 = 8;
+// var p3 = 9; var p4 = 1;
+// var media = (p1 + p2 + p3 + p4) / 4;
+// console.log(media)
+
+
+// let dolar = 20;
+// let conversão = dolar * 4.94;
+// resultado = conversão;
+
+// console.log(resultado)
+
+
+// let metros = 10;
+// let centimetros = 100;
+// let resul = metros * centimetros;
+
+// console.log(resul)
+
+
+document.getElementById('demo').innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
