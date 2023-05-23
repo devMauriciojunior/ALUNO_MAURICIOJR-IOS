00290ac63f342393e67795bd3189beb4d02ece00
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-05-23 10:24:37 -0300

    Aula 7 e 8 - Elementos html em js & Switch

---
+++ JS/7° Aula - HTML Elements/script.js
@@ -0,0 +1,7 @@
+let TitleH2 = document.createElement("h1")
+document.body.appendChild(TitleH2)
+// TitleH2.innerHTML = "HELLO WORLD"
+
+let FirstText = document.createTextNode(" MauMau lindão")
+// document.body.appendChild(FirstText)
+TitleH2.appendChild(FirstText)
