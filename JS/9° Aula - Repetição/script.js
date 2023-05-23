933b913d74fe222d79c97f95059eaee4e693eaa9
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-05-23 11:45:31 -0300

    Loops

---
+++ JS/9° Aula - Repetição/script.js
@@ -0,0 +1,18 @@
+// let x = 1
+// console.log(x)
+// x = x + 1 
+// console.log(x)
+// x = x + 1
+// console.log(x)
+
+
+
+let a = 0
+
+while (a < 10) {
+    let b = 3
+    let c = a * b
+    console.log(`${b} x ${a} = ${c}`)
+    a++
+}
+
