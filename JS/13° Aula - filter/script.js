7d245f045e60c1b3655c35369256c8527b99ff94
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-05-30 10:04:11 -0300

    filter

---
+++ JS/13° Aula - filter/script.js
@@ -0,0 +1,14 @@
+let idade = [20, 19, 17, 26, 27];
+console.log(idade)
+
+// Filter() cria um array com o resultado do filter aplicado pelo método, os elementos que "passaram" na condição determinado pelo no filtro.
+
+let mostrar = document.getElementById("demonstracao");
+mostrar.innerHTML = idade.filter(Maiordeidade)
+
+function Maiordeidade(idade){
+    return idade > 17;
+}
+
+console.log(filter(Maiordeidade))
+console.log(idade)
