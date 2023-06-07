be7aa69effdadfcd4b6c7d6babc424b9417fc274
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-06-07 11:47:20 -0300

    Objetos e Class

---
+++ JS/20° Aula - Classes/script.js
@@ -0,0 +1,16 @@
+class NomeClasse {
+    constructor() {
+    }
+
+}
+
+class Carro {
+    constructor(nome, ano) {
+        this.nome = nome;
+        this.ano = ano;
+    }
+}
+
+let meuCarro1 = new Carro("Lamborghini", 2023)
+
+console.log(meuCarro1)
--- JS/20° Aula - Objetos/script.js
+++ JS/20° Aula - Objetos/script.js
@@ -18,3 +18,41 @@ mostrar.innerHTML = `<p>Nome: ${Aluno.PrimeiroNome}</p>`
 mostrar.innerHTML += `<p>Nome: ${Aluno.SegundoNome}</p>`
 mostrar.innerHTML += `<p>Idade: ${Aluno.idade}</p>`
 mostrar.innerHTML += `<p>Nacionalidade: ${Aluno.Nacionalidade}</p>`
+
+
+const pessoa = {
+    primeiroNome: 'Bruno',
+    segundoNome: 'Ruiz',
+    id: 3563535,
+    fullnome: function(){
+        return `${this.primeiroNome} ${this.segundoNome}`
+    }
+}
+mostrar.innerHTML += `<p>${pessoa.fullnome()}</p>`
+
+
+let cor = ["Azul", "preto"]
+
+const tarefas = [
+    {
+        id: 1, 
+        texto: "Levar o lixo",
+        completado: true,
+    },
+    {
+        id: 2, 
+        texto: "Encontrar com chefe",
+        completado: true,
+    },
+    {
+        id: 3, 
+        texto: "Alomoçar",
+        completado: false,
+    }
+]
+console.log(tarefas)
+tarefas.forEach(teste)
+
+function teste(tarefas){
+    console.log(tarefas.texto)
+}
