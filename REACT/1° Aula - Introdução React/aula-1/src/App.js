f10eb34532f20036f2a32d8960b6c5fadf7f4c7e
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-06-12 11:47:30 -0300

    REACT

---
+++ REACT/1° Aula - Introdução React/aula-1/src/App.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav className="navegacao">
+          <ul>
+            <li>MauMau</li>
+            <li>MauMau</li>
+            <li>MauMau</li>
+            <li>MauMau</li>
+            <li>MauMau</li>
+            <li>MauMau</li>
+          </ul>
+        </nav>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
