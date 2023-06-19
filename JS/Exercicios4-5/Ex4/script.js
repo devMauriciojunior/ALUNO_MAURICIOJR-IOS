e8c745b7658a21f0738e66ded2850e7a2a3ac24e
Mauricio JR Ios <Mjunior.dev@outlook.com>
2023-06-19 10:28:23 -0300

    refatoração

--- JS/Exercicios4-5/Ex4/script.js
+++ JS/Exercicios4-5/Ex4/script.js
@@ -9,8 +9,8 @@ function Media() {
     const media = (parseInt(N1.value) + parseInt(N2.value) + parseInt(N3.value)) / 3;
 
     if(media >= 7) {
-        resultado.innerHTML = `<span class="nome">${Nome.value}</span> você foi <span class="aprovado">aprovado</span> com média acima de 7, sua média foi: <span class="aprovado">${Math.floor(media)}</span>.`
+        resultado.innerHTML = `<span class="nome">${Nome.value}</span> você foi <span class="aprovado">aprovado</span> com média acima de 7, sua média foi: <span class="aprovado">${media.toFixed(2)}</span>.`
     } else {
-        resultado.innerHTML = `<span class="nome">${Nome.value}</span> você foi <span class="reprovado">reprovado</span> com média abaixo de 7, sua média foi: <span class="reprovado">${Math.floor(media)}</span>.`  
+        resultado.innerHTML = `<span class="nome">${Nome.value}</span> você foi <span class="reprovado">reprovado</span> com média abaixo de 7, sua média foi: <span class="reprovado">${media.toFixed(2)}</span>.`  
     }
 };
